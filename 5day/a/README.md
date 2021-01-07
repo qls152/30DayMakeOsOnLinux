436d0bb220272afaa3a59f3ea9e8523b52f6bee4
qls <qls@example.com>
2021-01-07 11:13:04 +0800

    update 5day/a

---
+++ 5day/a/README.md
@@ -0,0 +1,5 @@
+# 说明
+本节中的font.h主要参考：
+https://github.com/cherishsir/ubuntu230os/blob/master/day5/kernel/font.h
+
+本节主要实现字体显示，gdt初始化deng
---
+++ 5day/a/kernel/naskfunc.h
@@ -0,0 +1,94 @@
+#define io_hlt() \
+     asm volatile( \
+        "hlt\n\t"  \
+    );
+
+#define io_cli() \
+    asm volatile( \
+        "cli\n\t" \
+    );
+
+#define io_sti() \
+    asm volatile(\
+        "sti"    \
+    );
+
+#define io_stihlt() \
+    asm volatile( \
+        "sti\n\t" \
+        "hlt\n\t" \
+    );
+
+#define io_in8(port) \
+    asm volatile(    \
+        "movl $0, %%eax \n\t" \
+        "inb %%dx, %%al \n\t"  \
+        :           \
+        :"d"(port)  \
+    );
+    
+#define io_in16(port) \
+    asm volatile(    \
+        "movl $0, %%eax \n\t" \
+        "inw %%dx, %%ax \n\t"  \
+        :           \
+        :"d"(port)  \
+    );
+
+#define io_in32(port) \
+    asm volatile(    \
+        "movl $0, %%eax \n\t" \
+        "inl %%dx, %%eax \n\t"  \
+        :           \
+        :"d"(port)  \
+    );
+
+#define io_out8(port, data) \
+    asm volatile( \
+        "outb %%al, %%dx \n\t" \
+        :                   \
+        :"a"(data), "d"(port)\
+    );
+
+#define io_out16(port, data) \
+    asm volatile( \
+        "outw %%ax, %%dx \n\t" \
+        :                   \
+        :"a"(data), "d"(port)\
+    );
+
+#define io_out32(port, data) \
+    asm volatile( \
+        "outb %0, %%dx \n\t" \
+        :                   \
+        :"a"(data), "d"(port)\
+    );
+
+static __inline int io_load_eflags(void) __attribute__((always_inline));
+static __inline void io_store_eflags(int) __attribute__((always_inline));
+
+
+static __inline int io_load_eflags(void) {
+    int eflags;
+    asm volatile(
+        "pushfl \n\t"
+        "popl %0"
+        :"=r"(eflags)
+    );
+
+    return eflags;
+}
+
+static __inline void io_store_eflags(int eflags) {
+    asm volatile(
+        "pushl %0 \n\t"
+        "popfl"
+        :
+        :"r"(eflags)
+    );
+}
+
+
+
+
+
